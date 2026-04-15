5bdc5e94cc115d9812bb4c3df3a1fa67669408a4
romanovsqd <romanovsqd@gmail.com>
2026-04-15 17:27:15 +0300

    feat(args): add report argument to parser

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--files", type=str, nargs="+", required=True)
+    parser.add_argument("--report", type=str, required=True)
     args = parser.parse_args()
 
 
