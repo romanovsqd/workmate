95970b122e14213471638d049cf52e3377b578d5
romanovsqd <romanovsqd@gmail.com>
2026-04-15 17:51:49 +0300

    feat: add csv reader, filter data from csv, display report
    - Added csv DictReader to read rows from --flies args as dict
    - Added if statement to filter data and get only clickbate videos
    - Added tabulate print to display report

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import argparse
+import csv
+
+from tabulate import tabulate
 
 
 def main():
@@ -6,7 +9,30 @@ def main():
 
     parser.add_argument("--files", type=str, nargs="+", required=True)
     parser.add_argument("--report", type=str, required=True)
+
     args = parser.parse_args()
+
+    rows = []
+    for file_path in args.files:
+        with open(file_path, "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                title = row["title"]
+                ctr = float(row["ctr"])
+                retention_rate = float(row["retention_rate"])
+
+                if ctr > 15 and retention_rate < 40:
+                    rows.append(
+                        {
+                            "title": title,
+                            "ctr": ctr,
+                            "retention_rate": retention_rate,
+                        }
+                    )
+                    
+    rows.sort(key=lambda x: x["ctr"], reverse=True)
+
+    print(tabulate(rows, headers="keys", tablefmt="grid"))
 
 
 if __name__ == "__main__":
