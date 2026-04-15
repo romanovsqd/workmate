4963838dedfb97a786f0a221d098ebb46109169f
romanovsqd <romanovsqd@gmail.com>
2026-04-15 17:23:55 +0300

    feat(args): add parser for files argument

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--files", type=str, nargs="+", required=True)
+    args = parser.parse_args()
+
+
 if __name__ == "__main__":
-    pass
+    main()
